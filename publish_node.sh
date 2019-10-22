400c2ce1c638be40bb5cb9d0294f0f504142ed39
sergey radionov <rsatom@gmail.com>
2019-10-22 12:04:07 +0700

    added script for publish prebuilt binary

---
+++ publish_node.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+./build_node.sh
+
+rm -r ./dist || true
+mkdir dist
+
+cp ./build/Release/wcjs-gs.node ./dist/
+
+cat <<EOF > ./dist/index.js
+module.exports = ("./wcjs-gs.node");
+EOF
+
+cat <<EOF > ./dist/package.json
+{
+  "name": "wcjs-gs-prebuilt",
+  "description": "WebChimera.js GStreamer edition",
+  "version": "`node -p "require('./package.json').version"`",
+  "license": "LGPL-2.1",
+  "author": "Sergey Radionov <rsatom@gmail.com>",
+  "keywords": [
+    "wcjs",
+    "gstreamer"
+  ],
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/RSATom/wcjs-gs"
+  }
+}
+EOF
+
+cd dist
+npm publish
+
+cd -
